0b0545e159ab84c51cdee160923e81b5e511798e
fahmid <fahmidhasankonok@gmail.com>
2024-02-19 23:26:29 +0600

    fast commit

---
+++ resources/views/components/admin-footer-js.blade.php
@@ -0,0 +1,13 @@
+    <script src="{{asset('asset/js/bootstrap.bundle.min.js')}}"></script>
+	<!--plugins-->
+	<script src="{{asset('asset/js/jquery.min.js')}}"></script>
+	<script src="{{asset('asset/plugins/simplebar/js/simplebar.min.js')}}"></script>
+	<script src="{{asset('asset/plugins/metismenu/js/metisMenu.min.js')}}"></script>
+	<script src="{{asset('asset/plugins/perfect-scrollbar/js/perfect-scrollbar.js')}}"></script>
+	<script src="{{asset('asset/plugins/vectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+    <script src="{{asset('asset/plugins/vectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+	<script src="{{asset('asset/plugins/chartjs/js/Chart.min.js')}}"></script>
+	<script src="{{asset('asset/plugins/chartjs/js/Chart.extension.js')}}"></script>
+	<script src="{{asset('asset/js/index.js')}}"></script>
+	<!--app JS-->
+	<script src="{{asset('asset/js/app.js')}}"></script>
